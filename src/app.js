b887067f069bb6a4d394459649d400fa0b7d6d21
irchuk67 <foxmaster110518@gmail.com>
2022-09-21 23:43:54 +0300

    Add env file

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require("cors");
 const mongoose = require('mongoose');
-
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://receipts:receipts@localhost:27017/receipts');
 
