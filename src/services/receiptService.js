aacf6875812538a3d9738a8742d1e01f4b6bf607
irchuk67 <foxmaster110518@gmail.com>
2023-01-23 19:33:36 +0200

    add docs config

--- src/services/receiptService.js
+++ src/services/receiptService.js
@@ -1,19 +1,42 @@
 const mongoose = require('mongoose');
-const {response} = require("express");
-const {NO_CONTENT} = require("../constants/HTTPCodes");
 const Receipt = mongoose.model('Receipt');
 
+/**
+ * <h2>Get list of receipts</h2>
+ *
+ * This method queries the mongoDB to find list of all objects written by Receipt schema
+ *
+ * @returns list of {@link Receipt}
+ * @author Iryna Sadovska
+ * */
 function fetchReceipts(){
     return Receipt.find({})
 }
 
+/**
+ * <h2>Get receipt by id</h2>
+ *
+ * This method queries the mongoDB to find receipt objects with given id
+ *
+ * @param {number} id Receipt id to find
+ * @returns {@link Receipt} by given id
+ * @author Iryna Sadovska
+ * */
 function fetchReceiptById(id){
     return Receipt.findById(id)
 }
 
+/**
+ * <h2>Delete receipt</h2>
+ *
+ * This method queries the mongoDB to find receipt by id and delete it
+ *
+ * @param {number} id Receipt id to delete
+ * @returns {null} if receipt wasn`t found || object { acknowledged: true, deletedCount: {@link number} }
+ * @author Iryna Sadovska
+ * */
 function deleteReceipt(id){
     return Receipt.findById(id).then(response => {
-        console.log(response)
         if (!response) return null;
         else {
             return Receipt.deleteOne(response)
@@ -22,6 +45,15 @@ function deleteReceipt(id){
     })
 }
 
+/**
+ * <h2>Create new receipt</h2>
+ *
+ * This method queries the mongoDB to save new receipt
+ *
+ * @param {object} newReceipt Object that fields will be written to new {@link Receipt}
+ * @returns {@link Receipt} that was created
+ * @author Iryna Sadovska
+ * */
 function createReceipt(newReceipt){
     const receiptToCreate = new Receipt({
         text: newReceipt.text
@@ -30,6 +62,16 @@ function createReceipt(newReceipt){
 
 }
 
+/**
+ * <h2>Update receipt</h2>
+ *
+ * This method queries the mongoDB to find receipt by id and update it
+ *
+ * @param {number} id Receipt id to update
+ * @param {object} updatedReceipt Object that fields will be written to found by id {@link Receipt}
+ * @returns {null} if receipt wasn`t found || saved updated object
+ * @author Iryna Sadovska
+ * */
 function updateReceipt(id, updatedReceipt) {
     return Receipt.findById(id)
         .then(receipt => {
